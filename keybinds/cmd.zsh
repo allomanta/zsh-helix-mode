42fcc5efeb7b786b5c52b00279efc0b50bd186a5
Steven Lageveen <stevenmax888@gmail.com>
2024-10-14 17:40:16 +0200

    Most things working, mostly visual still needed to be usable

--- keybinds/cmd.zsh
+++ keybinds/cmd.zsh
@@ -5,48 +5,76 @@ function bind() {
 bind : execute-named-cmd
 
 # Change modes
-bind i vi-insert
-bind a vi-add-next
+bind i _hx-insert
+bind a _hx-add-next
 bind v _hx-vis-mode
+bind I .vi-insert-bol
+bind A .vi-add-eol
 
 # Char movement
-bind h _wrapped-vi-backward-char
-bind j _wrapped-down-line-or-history
-bind k _wrapped-up-line-or-history
-bind l _wrapped-vi-forward-char
+bind h _hx-backward-char
+bind j _hx-down-line-or-history
+bind k _hx-up-line-or-history
+bind l _hx-forward-char
 
 # Word movement
-bind w _wrapped-vi-forward-word
-bind e _wrapped-vi-forward-word-end
+bind w _hx-forward-word
+bind W _hx-forward-blank-word
+bind e _hx-forward-word-end
+bind E _hx-forward-blank-word-end
+bind b _hx-backward-word
+bind B _hx-backward-blank-word
 
 # Line movement
-bind "gg" _wrapped-beginning-of-buffer-or-history
-bind "gs" _wrapped-beginning-of-line
+bind "gg" _hx-beginning-of-buffer-or-history
+bind "ge" _hx-end-of-buffer-or-history
+bind "gs" _hx-first-non-blank
+bind "gh" _hx-beginning-of-line
+bind "gl" _hx-end-of-line
+
+# Match brackets
+bind "mm" .vi-match-bracket
+
 
 # Find
-bind f vi-find-next-char
-bind F vi-find-prev-char
-bind t vi-find-next-char-skip
-bind T vi-find-prev-char-skip
-bind n vi-repeat-search
-bind N vi-rev-repeat-search
+bind f .vi-find-next-char
+bind F .vi-find-prev-char
+bind t .vi-find-next-char-skip
+bind T .vi-find-prev-char-skip
+bind n .vi-repeat-search
+bind N .vi-rev-repeat-search
 
 # Replace
-bind r vi-replace-chars
+bind r .vi-replace-chars
+bind R _hx-paste-replace-selection
 
 # Copy Paste
-bind y vi-yank
-bind p vi-put-after
+bind y _hx-yank
+bind " y" _hx-yank-to-clipboard
+bind p _hx-paste
+bind P .vi-put-before
+bind " p" _hx-paste-from-clipboard
+bind " P" _hx-paste-before-from-clipboard
 
 # Lines
-bind o vi-open-line-below 
-bind O vi-open-line-above
+bind o .vi-open-line-below 
+bind O .vi-open-line-above
+bind J .vi-join
+bind '>' .vi-indent -n 1
+bind '<' .vi-unindent -n 1
+bind x _hx-select-line
+
 
 # misc
-bind ~ vi-swap-case
-bind u undo
-bind d vi-delete
+bind '^[[200~' .bracketed-paste
+bind ~ .vi-swap-case
+bind u .undo
+bind U .redo
+bind d _hx-delete
+bind c _hx-change
+bind "^M" .accept-line
+bind '^[' _hx-unselect
 
-autoload -Uz edit-command-line
-zle -N edit-command-line
-bind m edit-command-line
+autoload -Uz .edit-command-line
+zle -N .edit-command-line
+bind m .edit-command-line
--- widgets/common/hx-command-mode.zsh
+++ widgets/common/hx-command-mode.zsh
@@ -1,59 +1,194 @@
-bef_wrap=(
-  vi-forward-word 
-  vi-forward-word-end
-)
-
-for wrapee in $bef_wrap; do
-  name=_wrapped-"$wrapee"
-  eval "$name() {
-    zle set-mark-command
-    zle $wrapee
-  }"
-  zle -N $name
-done
-
-aft_wrap=(
-  vi-forward-char
+_wrap=(
   end-of-line
   beginning-of-line
+  end-of-line
   beginning-of-buffer-or-history
+  end-of-buffer-or-history
   down-line-or-history
   up-line-or-history
   vi-backward-char
+  vi-first-non-blank
 )
 
-for wrapee in $aft_wrap; do
-  name=_wrapped-"$wrapee"
+for wrapee in $_wrap; do
+  name="_hx-$(echo $wrapee | sed 's/vi-//g')"
   eval "$name() {
-    zle $wrapee
-    zle set-mark-command
+    zle ."$wrapee"
+    zle .set-mark-command
   }"
   zle -N $name
 done
+
+_hx-insert() {
+  if [[ $MARK -lt $CURSOR ]]; then
+    zle exchange-point-and-mark
+  fi
+  zle .vi-insert
+}
+zle -N _hx-insert
+
+_hx-add-next() {
+  if [[ $MARK -gt $CURSOR ]]; then
+    zle exchange-point-and-mark
+    (( --CURSOR ))
+  fi
+  zle .vi-add-next
+}
+zle -N _hx-add-next
+
+_hx-forward-char() {
+  zle vi-forward-char
+  zle .set-mark-command
+}
+zle -N _hx-forward-char
+
+_hx-forward-word() {
+  zle .set-mark-command
+  zle .vi-forward-word
+  ((--CURSOR))
+}
+zle -N _hx-forward-word
+
+_hx-forward-blank-word() {
+  zle .set-mark-command
+  zle .vi-forward-blank-word
+  ((--CURSOR))
+}
+zle -N _hx-forward-blank-word
+
+
+_hx-forward-word-end() {
+  zle .set-mark-command
+  zle .vi-forward-word-end
+}
+zle -N _hx-forward-word-end
+
+_hx-forward-blank-word-end() {
+  zle .set-mark-command
+  zle .vi-forward-blank-word-end
+}
+zle -N _hx-forward-blank-word-end
+
+_hx-backward-word() {
+  ((++CURSOR))
+  zle .set-mark-command
+  ((--CURSOR))
+  zle .vi-backward-word
+}
+zle -N _hx-backward-word
+
+_hx-backward-blank-word() {
+  ((++CURSOR))
+  zle .set-mark-command
+  ((--CURSOR))
+  zle .vi-backward-blank-word
+}
+zle -N _hx-backward-blank-word
+
+_hx-yank() {
+  if [[ $MARK -le $CURSOR ]]; then
+    ((++CURSOR))
+    zle .copy-region-as-kill
+    ((--CURSOR))
+  else
+    zle .copy-region-as-kill
+  fi
+}
+zle -N _hx-yank
+
+_hx-yank-to-clipboard() {
+  if [[ $MARK -le $CURSOR ]]; then
+    wl-copy <<< ${BUFFER:$MARK:$((CURSOR-MARK+1))}
+  else
+    echo huh ${BUFFER:$CURSOR:$((CURSOR-MARK+1))}
+    wl-copy <<< ${BUFFER:$CURSOR:((MARK-CURSOR+1))}
+  fi
+}
+zle -N _hx-yank-to-clipboard
+
+_hx-paste() {
+  zle .vi-put-after
+  (( --CURSOR ))
+}
+zle -N _hx-paste
+
+_hx-paste-replace-selection() {
+  if [[ $MARK -le $CURSOR ]]; then
+    ((++CURSOR))
+    zle .put-replace-selection
+    ((--CURSOR))
+  else
+    zle .put-replace-selection
+  fi
+}
+zle -N _hx-paste-replace-selection
+
+_hx-paste-from-clipboard() {
+  local text=$(wl-paste)
+  if [[ $MARK -le $CURSOR ]]; then
+    ((++CURSOR))
+    LBUFFER+=$text
+    ((--CURSOR))
+  else
+    BUFFER=${BUFFER:0:$MARK}$text${BUFFER:$MARK}
+    MARK+=${#text}
+  fi
+}
+zle -N _hx-paste-from-clipboard
+
+_hx-paste-before-from-clipboard() {
+  local text=$(wl-paste)
+  if [[ $MARK -le $CURSOR ]]; then
+    BUFFER=${BUFFER:0:$MARK}$text${BUFFER:$MARK}
+    CURSOR=$CURSOR+${#text}
+  else
+    BUFFER=${BUFFER:0:$CURSOR}$text${BUFFER:$CURSOR}
+    MARK=$MARK+${#text}
+  fi
+}
+zle -N _hx-paste-before-from-clipboard
+
+_hx-delete() {
+  if [[ $MARK -le $CURSOR ]]; then
+    ((++CURSOR))
+    zle .kill-region
+    ((--CURSOR))
+  else
+    zle .kill-region
+  fi
+}
+zle -N _hx-delete
+
+_hx-change() {
+  if [[ $MARK -le $CURSOR ]]; then
+    ((++CURSOR))
+    zle .kill-region
+    ((--CURSOR))
+    zle .vi-insert
+  else
+    zle .vi-change
+  fi
+}
+zle -N _hx-change
+
+_hx-select-line() {
+  MARK=0
+  zle .end-of-line
+}
+zle -N _hx-select-line 
+
+_hx-unselect() {
+  MARK=$CURSOR
+}
+zle -N _hx-unselect 
 
 _hx-cmd-mode() {
   zle -K hxcmd
 }
-
 zle -N _hx-cmd-mode 
-
 
 _hx-vis-mode() {
   zle -K hxvis
 }
-
-
 zle -N _hx-vis-mode 
 
-test() {
-  echo "\n"
-  echo 1 $BUFFER
-  echo 2 $MARK
-  echo 3 $CURSOR
-  echo 4 $REGION_ACTIVE
-  echo 5 $region_highlight
-  echo 6 $region
-}
-
-
-zle -N test 
